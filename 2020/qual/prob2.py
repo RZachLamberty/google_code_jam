cba6bdd7b5572e6fb868fa2a9bc9725a11b9a93d
r.zach.lamberty <r.zach.lamberty@gmail.com>
2020-04-04 11:07:34 -0400

    [2020 qual round]: p2 final solution

--- 2020/qual/prob2.py
+++ 2020/qual/prob2.py
@@ -1,30 +1,14 @@
-DEBUG = False
-
-
-def debug(s):
-    if DEBUG:
-        print(s)
-
-
 def main():
     T = int(input())
 
     for x in range(1, T + 1):
         S = [int(_) for _ in input()]
 
-        debug(f'\n\nstarting case #{x}')
-        debug(f'S = {S}')
-
         y = ''
         last_val = 0
 
         for next_val in S:
             delta = last_val - next_val
-
-            debug(f'\ny = {y}')
-            debug(f'last_val = {last_val}')
-            debug(f'next_val = {next_val}')
-            debug(f'delta = {delta}')
 
             if delta == 0:
                 pass
@@ -35,8 +19,6 @@ def main():
 
             y += str(next_val)
             last_val = next_val
-
-            debug(f'y becomes: {y}')
 
         y += ')' * last_val
 
