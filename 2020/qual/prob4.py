1b5d7e27bf7de0e88ec5ae3b801257583fa6eb9a
r.zach.lamberty <r.zach.lamberty@gmail.com>
2020-04-05 09:29:59 -0400

    [2020 qual round]: p4 working answer before debug removed and submission

--- 2020/qual/prob4.py
+++ 2020/qual/prob4.py
@@ -8,185 +8,114 @@ def debug(s):
         print(s)
 
 
-def query(i):
+def query(i, qry_ctr):
     print(i)
     sys.stdout.flush()
 
     response = input()
+    qry_ctr += 1
     try:
-        return bool(int(response))
+        return bool(int(response)), qry_ctr
     except ValueError:
-        return response
+        return response, qry_ctr
 
 
-def is_reset_query(qry_idx):
-    return (qry_idx % 10 == 0) and (qry_idx > 0)
+SYMMETRIC = True
+ANTI_SYMMETRIC = False
+UNKNOWN = None
 
 
-def get_symm_pairs(bit_array, B):
-    qry_idx = 0
-    i_same = i_diff = None
+class BitQuerier():
+    def __init__(self, B):
+        self.B = B
+        self.bits = {i: UNKNOWN for i in range(1, self.B + 1)}
+        self.symmetry = {i: UNKNOWN for i in range(1, self.B + 1)}
 
-    for i_left in range(B // 2, 0, -1):
-        i_right = B - i_left + 1
+    @property
+    def any_unknown(self):
+        return UNKNOWN in self.bits.values()
 
-        # reset every 10
-        if is_reset_query(qry_idx):
-            for i in bit_array:
-                bit_array[i] = None
+    def get_unknown(self):
+        return min(k for (k, v) in self.bits.items() if v is UNKNOWN)
 
-        bit_array[i_left] = query(i_left)
-        bit_array[i_right] = query(i_right)
-        qry_idx += 2
+    @property
+    def symmetry_types(self):
+        return {v for v in self.symmetry.values() if v is not UNKNOWN}
 
-        if (i_same is None) and (bit_array[i_left] == bit_array[i_right]):
-            i_same = i_left
+    def get_symmetry_class(self, sym_class=SYMMETRIC):
+        return {k for (k, v) in self.symmetry.items() if v is sym_class}
 
-        if (i_diff is None) and (bit_array[i_left] != bit_array[i_right]):
-            i_diff = i_left
+    def get_symmetry_class_repr(self, sym_class=SYMMETRIC):
+        i = min(self.get_symmetry_class(sym_class))
+        v = self.bits[i]
+        assert v is not UNKNOWN
+        return i, v
 
-        if i_same and i_diff:
-            break
+    def flip_symmetry_class(self, sym_class=SYMMETRIC):
+        i_to_flip = self.get_symmetry_class(sym_class)
+        for i in i_to_flip:
+            curr_val = self.bits[i]
+            assert curr_val is not UNKNOWN
+            self.bits[i] = not curr_val
 
-    # leave here with the bit_array values set, hell or high water
-    num_to_get = 0
-    need_same = (i_same is not None) and bit_array[i_same] is None
-    need_diff = (i_diff is not None) and bit_array[i_diff] is None
-    if need_same:
-        num_to_get += 1
-    if need_diff:
-        num_to_get += 1
-
-    # make sure we won't trigger a fluctuation between the two
-    if num_to_get == 2 and (qry_idx % 10 == 9):
-        _ = query(1)
-        qry_idx += 1
-
-    if is_reset_query(qry_idx) and num_to_get > 0:
-        for i in bit_array:
-            bit_array[i] = None
-
-    if need_same:
-        i_flip = B - i_same + 1
-        bit_array[i_flip] = bit_array[i_same] = query(i_same)
-        qry_idx += 1
-
-    if need_diff:
-        i_flip = B - i_diff + 1
-        bit_array[i_diff] = query(i_diff)
-        qry_idx += 1
-        bit_array[i_flip] = not bit_array[i_diff]
-
-    return i_same, i_diff, qry_idx
-
-
-def get_updates(i_same, i_diff, bit_array):
-    qry_delta = 0
-    if i_same is None:
-        # (i_same is None) iff the array is entirely anti-symmetric. this means
-        # a bit flip is equivalent to a rotation. this means our four options
-        # collapse into only two
-        # (  ,  ) --> (  ,  )
-        # (BF,  ) --> (BF,  )
-        # (  , R) --> (BF,  )
-        # (BF, R) --> (  ,  )
-        # we can use the diff records to figure out which to choose
-        v_diff_prev = bit_array[i_diff]
-        v_diff_new = query(i_diff)
-        qry_delta += 1
-        is_bitflip = v_diff_prev != v_diff_new
-        is_reverse = False
-    else:
-        v_same_prev = bit_array[i_same]
-        v_same_new = query(i_same)
-        qry_delta += 1
-        is_bitflip = (v_same_prev != v_same_new)
-
-        # (i_diff is None) iff the array is entirely symmetric. a reverse is
-        # the same as doing nothing
-        if i_diff is None:
-            is_reverse = False
-        else:
-            v_diff_prev = bit_array[i_diff]
-            v_diff_new = query(i_diff)
-            qry_delta += 1
-            # enumerate the possibilities, you'll see it's true
-            is_reverse = (v_diff_prev == v_diff_new) == is_bitflip
-
-    return is_bitflip, is_reverse, qry_delta
-
-
-def apply_bitflip(bit_array):
-    for (i, v) in bit_array.items():
-        if v is not None:
-            bit_array[i] = not v
-
-
-def apply_reverse(bit_array, B):
-    return {B - i + 1: v for (i, v) in bit_array.items()}
-
-
-def bit_array_to_str(bit_array, B):
-    lookup = {True: '1', False: '0', None: '?'}
-    return ''.join([lookup[bit_array[i]] for i in range(1, B + 1)])
+    @property
+    def answer(self):
+        lookup = {True: '1', False: '0', UNKNOWN: '?'}
+        return ''.join([lookup[self.bits[i]] for i in range(1, self.B + 1)])
 
 
 def main():
     T, B = [int(_) for _ in input().split()]
 
     for test_idx in range(T):
-        bit_array = {i: None for i in range(1, B + 1)}
+        bc = BitQuerier(B)
 
-        # get both an equal and opposite symmetric pairs
-        i_same, i_diff, qry_idx = get_symm_pairs(bit_array, B)
+        # break everything up in sets of 10
+        for i in range(15):
+            qry_ctr = 0
 
-        is_symmetric = i_diff is None
-        is_anti_symmetric = i_same is None
+            # symmetric and anti-symmetric pairs all update the same way. we
+            # don't need to figure out what happened, we just now that if one
+            # (anti)symmetric pair flipped, all did
+            for sym_class in bc.symmetry_types:
+                i_prev, v_prev = bc.get_symmetry_class_repr(sym_class)
+                v_new, qry_ctr = query(i_prev, qry_ctr)
 
-        if is_symmetric and is_anti_symmetric:
-            raise ValueError(
-                f'found neither i_same nor i_diff for test_idx = {test_idx}')
+                if v_new != v_prev:
+                    bc.flip_symmetry_class(sym_class)
 
-        yet_unknowns = {k for (k, v) in bit_array.items() if v is None}
-        debug(f'yet_unknowns at start: {yet_unknowns}')
-        while qry_idx < 150 and yet_unknowns:
-            debug(f'qry_idx = {qry_idx}')
-            debug(f'bit_array = {bit_array_to_str(bit_array, B)}')
-            if is_reset_query(qry_idx):
-                is_bitflip, is_reverse, qry_delta = get_updates(i_same, i_diff,
-                                                                bit_array)
-                if is_bitflip:
-                    debug('doing bitflip')
-                    apply_bitflip(bit_array)
-                if is_reverse:
-                    debug('doing reverse')
-                    bit_array = apply_reverse(bit_array, B)
-                yet_unknowns = {k for (k, v) in bit_array.items() if v is None}
-                qry_idx += qry_delta
-            else:
-                i_q = yet_unknowns.pop()
-                bit_array[i_q] = query(i_q)
+            # if we only queried once, just burn a query (have to to keep pair
+            # queries balanced below
+            if qry_ctr == 1:
+                _, qry_ctr = query(1, qry_ctr)
 
-                i_flip = B - i_q + 1
-                if is_symmetric:
-                    bit_array[i_flip] = bit_array[i_q]
-                    yet_unknowns.discard(i_flip)
+            # with whatever is left of our 10 queries, gather data
+            while (qry_ctr < 10) and bc.any_unknown:
+                i_left = bc.get_unknown()
+                i_right = B - i_left + 1
+                v_left, qry_ctr = query(i_left, qry_ctr)
+                v_right, qry_ctr = query(i_right, qry_ctr)
 
-                if is_anti_symmetric:
-                    bit_array[i_flip] = not bit_array[i_q]
-                    yet_unknowns.discard(i_flip)
+                bc.bits[i_left] = v_left
+                bc.bits[i_right] = v_right
 
-                qry_idx += 1
+                bc.symmetry[i_left] = bc.symmetry[i_right] = (
+                    SYMMETRIC if v_left == v_right else ANTI_SYMMETRIC)
 
-        if qry_idx >= 150:
-            raise ValueError(f'yup. {bit_array_to_str(bit_array, B)}')
-            exit()
+            # two reasons to escape the above loop: we know everything or we
+            # are about to hit an update.
+            if not bc.any_unknown:
+                # finish this test
+                break
 
-        answer = bit_array_to_str(bit_array, B)
-
-        result = query(answer)
+        result, qry_ctr = query(bc.answer, qry_ctr)
         if result == 'N':
-            exit()
+            print('bc.answer = {}'.format(bc.answer), sys.stderr)
+            exit(bc.answer)
+        elif result == 'Y':
+            continue
+        else:
+            raise ValueError('how did we get result {}'.format(result))
 
 
 if __name__ == '__main__':
